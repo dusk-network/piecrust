herr-seppia/hatchery

Initial argument passing and host allocations

dallo/src/boxed.rs
@@ -1,0 +1,41 @@
+use core::ops::{Deref, DerefMut};
+
+use crate::memory::Memory;
+
+pub struct Box<T: ?Sized> {
+    ptr: *mut T,
+}
+
+impl<T: ?Sized> Drop for Box<T> {
+    fn drop(&mut self) {
+        Memory::release(self.ptr)
+    }
+}
+
+impl<T> Box<T> {
+    pub fn new(mut t: T) -> Self {
+        let mut ptr = Memory::request(1);
+        ptr.write(&mut t);
+        let ptr = unsafe { ptr.assume_init() };
+        Box { ptr }
+    }
+
+    pub const fn empty_slice() -> Box<[T]> {
+        let m: &mut [T] = &mut [];
+        Box { ptr: m }
+    }
+}
+
+impl<T> Deref for Box<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T> DerefMut for Box<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.ptr }
+    }
+}

dallo/src/guest_mem.rs
@@ -1,0 +1,23 @@
+use core::mem::MaybeUninit;
+
+extern "C" {
+    fn request(size: usize, align: usize) -> *mut u8;
+    fn release(ptr: *mut u8);
+}
+
+pub(crate) struct GuestMem;
+
+impl GuestMem {
+    pub fn request<T>(n: usize) -> MaybeUninit<*mut T> {
+        let size = core::mem::size_of::<T>() * n;
+        let align = core::mem::align_of::<T>();
+        let ptr = unsafe { request(size, align) };
+        let t: *mut T = unsafe { core::mem::transmute(ptr) };
+        MaybeUninit::new(t)
+    }
+
+    pub fn release<T>(ptr: *mut T) {
+        let ptr: *mut u8 = unsafe { core::mem::transmute(ptr) };
+        unsafe { release(ptr) }
+    }
+}

dallo/src/memory.rs
@@ -1,0 +1,28 @@
+use core::mem::MaybeUninit;
+
+pub(crate) struct Memory;
+
+impl Memory {
+    pub fn request<T>(n: usize) -> MaybeUninit<*mut T> {
+        {
+            #[cfg(feature = "host")]
+            crate::host_mem::HostMem::request(n)
+        }
+        {
+            #[cfg(not(feature = "host"))]
+            crate::guest_mem::GuestMem::request(n)
+        }
+    }
+
+    pub fn release<T: ?Sized>(ptr: *mut T) {
+        let byteptr: *mut u8 = ptr.cast();
+        {
+            #[cfg(feature = "host")]
+            crate::host_mem::HostMem::release(byteptr)
+        }
+        {
+            #[cfg(not(feature = "host"))]
+            crate::guest_mem::GuestMem::release(byteptr)
+        }
+    }
+}

dusk-network/piecrust

dallo/src/boxed.rs
@@ -1,0 +1,41 @@
+use core::ops::{Deref, DerefMut};
+
+use crate::memory::Memory;
+
+pub struct Box<T: ?Sized> {
+    ptr: *mut T,
+}
+
+impl<T: ?Sized> Drop for Box<T> {
+    fn drop(&mut self) {
+        Memory::dealloc(self.ptr)
+    }
+}
+
+impl<T> Box<T> {
+    pub fn new(mut t: T) -> Self {
+        let mut ptr = Memory::alloc(1);
+        ptr.write(&mut t);
+        let ptr = unsafe { ptr.assume_init() };
+        Box { ptr }
+    }
+
+    pub const fn empty_slice() -> Box<[T]> {
+        let m: &mut [T] = &mut [];
+        Box { ptr: m }
+    }
+}
+
+impl<T> Deref for Box<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T> DerefMut for Box<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.ptr }
+    }
+}

dallo/src/guest_mem.rs
@@ -1,0 +1,23 @@
+use core::mem::MaybeUninit;
+
+extern "C" {
+    fn alloc(size: usize, align: usize) -> *mut u8;
+    fn dealloc(ptr: *mut u8);
+}
+
+pub(crate) struct GuestMem;
+
+impl GuestMem {
+    pub fn alloc<T>(n: usize) -> MaybeUninit<*mut T> {
+        let size = core::mem::size_of::<T>() * n;
+        let align = core::mem::align_of::<T>();
+        let ptr = unsafe { alloc(size, align) };
+        let t: *mut T = unsafe { core::mem::transmute(ptr) };
+        MaybeUninit::new(t)
+    }
+
+    pub fn dealloc<T>(ptr: *mut T) {
+        let ptr: *mut u8 = unsafe { core::mem::transmute(ptr) };
+        unsafe { dealloc(ptr) }
+    }
+}

dallo/src/memory.rs
@@ -1,0 +1,28 @@
+use core::mem::MaybeUninit;
+
+pub(crate) struct Memory;
+
+impl Memory {
+    pub fn alloc<T>(n: usize) -> MaybeUninit<*mut T> {
+        {
+            #[cfg(feature = "host")]
+            crate::host_mem::HostMem::alloc(n)
+        }
+        {
+            #[cfg(not(feature = "host"))]
+            crate::guest_mem::GuestMem::alloc(n)
+        }
+    }
+
+    pub fn dealloc<T: ?Sized>(ptr: *mut T) {
+        let byteptr: *mut u8 = ptr.cast();
+        {
+            #[cfg(feature = "host")]
+            crate::host_mem::HostMem::dealloc(byteptr)
+        }
+        {
+            #[cfg(not(feature = "host"))]
+            crate::guest_mem::GuestMem::dealloc(byteptr)
+        }
+    }
+}