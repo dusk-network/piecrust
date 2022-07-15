44777216293f50ca5e35f5c61b861880eee3cb96
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-07-15 12:56:48 +0200

    Change call center to take an ID as argument

--- tests/callcenter.rs
+++ tests/callcenter.rs
@@ -26,29 +26,26 @@ pub fn world_center_counter() -> Result<(), Error> {
     let mut world = World::new();
 
     let counter_id = world.deploy(module_bytecode!("counter"))?;
-    assert_eq!(&counter_id, include_bytes!("../modules/counter/id"));
 
+    // read value directly
     let value: i64 = world.query(counter_id, "read_value", ())?;
     assert_eq!(value, 0xfc);
 
     let center_id = world.deploy(module_bytecode!("callcenter"))?;
-    assert_eq!(&center_id, include_bytes!("../modules/callcenter/id"));
 
     // read value through callcenter
-
-    let value: i64 = world.query(center_id, "query_counter", ())?;
+    let value: i64 = world.query(center_id, "query_counter", counter_id)?;
     assert_eq!(value, 0xfc);
 
+    // increment through call center
     world.transact(center_id, "increment_counter", ())?;
 
-    // read back without proxy.
-
+    // read value directly
     let value: i64 = world.query(counter_id, "read_value", ())?;
     assert_eq!(value, 0xfd);
 
-    // read back with proxy.
-
-    let value: i64 = world.query(center_id, "query_counter", ())?;
+    // read value through callcenter
+    let value: i64 = world.query(center_id, "query_counter", counter_id)?;
     assert_eq!(value, 0xfd);
 
     Ok(())
