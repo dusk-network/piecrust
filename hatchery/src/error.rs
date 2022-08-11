24981b01edcbb09bf40099b052f341674e8276fc
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-08-11 15:53:07 +0200

    Add points usage test

--- hatchery/src/error.rs
+++ hatchery/src/error.rs
@@ -25,6 +25,7 @@ pub enum Error {
     MissingModuleExport,
     CompositeSerializerError(Compo),
     PersistenceError(io::Error),
+    OutOfPoints,
 }
 
 impl From<wasmer::InstantiationError> for Error {
