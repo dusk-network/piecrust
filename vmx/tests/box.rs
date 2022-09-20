cb53f1df36655a285ccce5b04ded43f6628ec6fb
Kristoffer Ström <kristoffer@rymdkoloni.se>
2022-09-20 21:52:33 +0200

    print comment

--- vmx/tests/box.rs
+++ vmx/tests/box.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 use vmx::{module_bytecode, Error, VM};
 
 #[test]
@@ -9,6 +15,8 @@ pub fn box_set_get() -> Result<(), Error> {
     let value: Option<i16> = vm.query(id, "get", ())?;
 
     assert_eq!(value, None);
+
+    println!("no problem until here");
 
     let mut session = vm.session();
 
