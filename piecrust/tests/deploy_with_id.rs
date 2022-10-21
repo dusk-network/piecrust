6c2cb57cf100a23d95c21bf0a6f8ed6a64f6e985
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-10-21 16:29:58 +0200

    Test for deploy_with_id

---
+++ piecrust/tests/deploy_with_id.rs
@@ -0,0 +1,28 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use piecrust::{module_bytecode, Error, VM};
+use piecrust_uplink::ModuleId;
+
+#[test]
+pub fn deploy_with_id() -> Result<(), Error> {
+    let mut vm = VM::ephemeral()?;
+
+    let bytecode = module_bytecode!("counter");
+    let some_id = [1u8; 32];
+    let module_id = ModuleId::from(some_id);
+    vm.deploy_with_id(module_id, bytecode)?;
+
+    let mut session = vm.session();
+
+    assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfc);
+
+    session.transact::<(), ()>(module_id, "increment", ())?;
+
+    assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfd);
+
+    Ok(())
+}
