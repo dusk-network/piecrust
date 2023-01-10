f85209f817b0f7b7593b77d20cedc56d322fd92e
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-01-10 19:14:02 +0100

    Refactored file paths, eliminated static path

--- cold_reboot_test/src/main.rs
+++ cold_reboot_test/src/main.rs
@@ -7,13 +7,14 @@
 extern crate core;
 
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use piecrust::{CommitId, Session, VM};
 
-static mut PATH: String = String::new();
-
-fn initialize(session: &mut Session) -> Result<(), piecrust::Error> {
+fn initialize<P: AsRef<Path>>(
+    session: &mut Session,
+    commit_id_file_path: P,
+) -> Result<(), piecrust::Error> {
     let counter_bytecode = include_bytes!(
         "../../target/wasm32-unknown-unknown/release/counter.wasm"
     );
@@ -26,16 +27,17 @@ fn initialize(session: &mut Session) -> Result<(), piecrust::Error> {
 
     assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfd);
 
-    let commit_id_path = PathBuf::from(unsafe { &PATH }).join("commit_id");
     let commit_id = session.commit()?;
-    commit_id.persist(commit_id_path)?;
+    commit_id.persist(commit_id_file_path)?;
 
     Ok(())
 }
 
-fn confirm(session: &mut Session) -> Result<(), piecrust::Error> {
-    let file_path = PathBuf::from(unsafe { &PATH }).join("commit_id");
-    let commit_id = CommitId::from(file_path)?;
+fn confirm<P: AsRef<Path>>(
+    session: &mut Session,
+    commit_id_file_path: P,
+) -> Result<(), piecrust::Error> {
+    let commit_id = CommitId::from(commit_id_file_path)?;
     session.restore(&commit_id)?;
 
     let counter_bytecode = include_bytes!(
@@ -59,21 +61,20 @@ fn main() -> Result<(), piecrust::Error> {
         return Ok(());
     }
 
-    let mut vm: VM = unsafe {
-        PATH = args[1].clone();
-        VM::new(&PATH)?
-    };
+    let vm_data_path = args[1].clone();
+    let commit_id_file_path = PathBuf::from(&vm_data_path).join("commit_id");
+    let mut vm = VM::new(&vm_data_path)?;
     let mut session = vm.session();
 
-    initialize(&mut session)?;
+    initialize(&mut session, &commit_id_file_path)?;
 
     vm.persist()?;
 
     /*
      * Simulate cold reboot - construct new VM in the same location.
      */
-    let mut vm_rebooted: VM = unsafe { VM::new(&PATH)? };
+    let mut vm_rebooted: VM = VM::new(&vm_data_path)?;
     let mut session_rebooted = vm_rebooted.session();
 
-    confirm(&mut session_rebooted)
+    confirm(&mut session_rebooted, &commit_id_file_path)
 }
