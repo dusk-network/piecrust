59aafc17476c49886523df991a46431902e4b1a0
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-01-10 19:00:27 +0100

    Cold reboot test passing

--- cold_reboot_test/src/main.rs
+++ cold_reboot_test/src/main.rs
@@ -6,60 +6,25 @@
 
 extern crate core;
 
-use std::borrow::Cow;
 use std::env;
-use std::error::Error;
-use std::fmt::Display;
-use std::fs;
 use std::path::PathBuf;
 
-use piecrust::{CommitId, ModuleId, Session, VM};
+use piecrust::{CommitId, Session, VM};
 
 static mut PATH: String = String::new();
-
-#[derive(Debug)]
-struct IllegalArg;
-impl Error for IllegalArg {}
-
-impl Display for IllegalArg {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Illegal arg")
-    }
-}
-
-#[derive(Debug)]
-struct PersistE;
-impl Error for PersistE {}
-
-impl Display for PersistE {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Persist Error")
-    }
-}
 
 fn initialize(session: &mut Session) -> Result<(), piecrust::Error> {
     let counter_bytecode = include_bytes!(
         "../../target/wasm32-unknown-unknown/release/counter.wasm"
     );
 
-    let counter_module_id = session.deploy(counter_bytecode)?;
+    let module_id = session.deploy(counter_bytecode)?;
 
-    assert_eq!(
-        session.query::<(), i64>(counter_module_id, "read_value", ())?,
-        0xfc
-    );
+    assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfc);
 
-    session.transact::<(), ()>(counter_module_id, "increment", ())?;
+    session.transact::<(), ()>(module_id, "increment", ())?;
 
-    assert_eq!(
-        session.query::<(), i64>(counter_module_id, "read_value", ())?,
-        0xfd
-    );
-
-    let module_id_path =
-        PathBuf::from(unsafe { &PATH }).join("counter_module_id");
-    fs::write(&module_id_path, counter_module_id.as_bytes())
-        .map_err(|e| piecrust::Error::PersistenceError(e))?;
+    assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfd);
 
     let commit_id_path = PathBuf::from(unsafe { &PATH }).join("commit_id");
     let commit_id = session.commit()?;
@@ -73,18 +38,15 @@ fn confirm(session: &mut Session) -> Result<(), piecrust::Error> {
     let commit_id = CommitId::from(file_path)?;
     session.restore(&commit_id)?;
 
-    let contract_module_id_path =
-        PathBuf::from(unsafe { &PATH }).join("counter_module_id");
-    let buf = fs::read(&contract_module_id_path)
-        .map_err(|e| piecrust::Error::RestoreError(e))?;
-    let mut bytes = [0u8; 32];
-    bytes.copy_from_slice(buf.as_ref());
-    let counter_module_id = ModuleId::from(bytes);
-
-    assert_eq!(
-        session.query::<(), i64>(counter_module_id, "read_value", ())?,
-        0xfd
+    let counter_bytecode = include_bytes!(
+        "../../target/wasm32-unknown-unknown/release/counter.wasm"
     );
+    /*
+     * Note that module deployment does not change its state.
+     */
+    let module_id = session.deploy(counter_bytecode)?;
+
+    assert_eq!(session.query::<(), i64>(module_id, "read_value", ())?, 0xfd);
 
     Ok(())
 }
@@ -92,24 +54,26 @@ fn confirm(session: &mut Session) -> Result<(), piecrust::Error> {
 fn main() -> Result<(), piecrust::Error> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        println!("expected: <path> (initialize|confirm|test_both)");
+    if args.len() < 2 {
+        println!("argument expected: <path_for_vm_data>");
         return Ok(());
     }
 
     let mut vm: VM = unsafe {
         PATH = args[1].clone();
-        VM::new(&PATH).expect("Creating ephemeral VM should work")
+        VM::new(&PATH)?
     };
     let mut session = vm.session();
 
-    match &*args[2] {
-        "initialize" => initialize(&mut session),
-        "confirm" => confirm(&mut session),
-        "test_both" => {
-            initialize(&mut session)?;
-            confirm(&mut session)
-        }
-        _ => Err(piecrust::Error::SessionError(Cow::from("invalid argument"))),
-    }
+    initialize(&mut session)?;
+
+    vm.persist()?;
+
+    /*
+     * Simulate cold reboot - construct new VM in the same location.
+     */
+    let mut vm_rebooted: VM = unsafe { VM::new(&PATH)? };
+    let mut session_rebooted = vm_rebooted.session();
+
+    confirm(&mut session_rebooted)
 }
