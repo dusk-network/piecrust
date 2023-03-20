6748fb0a3510ed7dcd1b6bd75de57422702a124b
Milosz Muszynski <milosz.muszynski@gmail.com>
2023-03-20 10:34:04 +0100

    Test contract and test for the constructor

---
+++ modules/constructor/src/lib.rs
@@ -0,0 +1,61 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+//! Module that provides and example use of the constructor.
+
+#![feature(arbitrary_self_types)]
+#![no_std]
+
+use piecrust_uplink as uplink;
+use uplink::{ModuleId, State};
+
+/// Struct that describes the state of the Constructor module
+pub struct Constructor {
+    value: u8,
+}
+
+impl Constructor {
+    pub fn init(&mut self, value: u8) {
+        self.value = value;
+    }
+}
+
+/// Module ID, initialized by the host when the module is deployed
+#[no_mangle]
+static SELF_ID: ModuleId = ModuleId::uninitialized();
+
+/// State of the Constructor module
+static mut STATE: State<Constructor> = State::new(Constructor { value: 0x50 });
+
+impl Constructor {
+    /// Read the value of the constructor module state
+    pub fn read_value(&self) -> u8 {
+        self.value
+    }
+    /// Increment the value  by 1
+    pub fn increment(&mut self) {
+        let value = self.value + 1;
+        self.value = value;
+    }
+}
+
+/// Expose `Constructor::read_value()` to the host
+#[no_mangle]
+unsafe fn read_value(arg_len: u32) -> u32 {
+    uplink::wrap_query(arg_len, |_: ()| STATE.read_value())
+}
+
+/// Expose `Constructor::increment()` to the host
+#[no_mangle]
+unsafe fn increment(arg_len: u32) -> u32 {
+    uplink::wrap_transaction(arg_len, |_: ()| STATE.increment())
+}
+
+/// Expose `Constructor::init()` to the host
+#[no_mangle]
+unsafe fn init(arg_len: u32) -> u32 {
+    uplink::wrap_transaction(arg_len, |arg: u8| STATE.init(arg))
+}
---
+++ piecrust/tests/constructor.rs
@@ -0,0 +1,56 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use piecrust::{module_bytecode, Error, VM};
+
+#[test]
+fn constructor() -> Result<(), Error> {
+    let vm = VM::ephemeral()?;
+
+    let mut session = vm.genesis_session();
+
+    let id = session.deploy(module_bytecode!("constructor"))?;
+
+    assert_eq!(session.query::<(), u8>(id, "read_value", &())?, 0x50);
+
+    // perform transaction and make sure that it fails because the contract is
+    // not initialized yet
+    let result = session.transact::<(), ()>(id, "increment", &());
+    assert!(
+        result.is_err(),
+        "transaction on not initialized module should fail"
+    );
+
+    // initialize contract with some specific data
+    session.init::<u8>(id, &0xab)?;
+
+    // we should not be able to initialize contract more than once
+    let result = session.init::<u8>(id, &0xaa);
+    assert!(
+        result.is_err(),
+        "initializing more than once should not be allowed"
+    );
+
+    // perform transaction again, this time it should succeed
+    session.transact::<(), ()>(id, "increment", &())?;
+
+    // and make sure it performed ok
+    assert_eq!(session.query::<(), u8>(id, "read_value", &())?, 0xac);
+
+    // initialized state should live through across session boundaries
+    let commit_id = session.commit()?;
+    let mut session = vm.session(commit_id)?;
+    assert_eq!(session.query::<(), u8>(id, "read_value", &())?, 0xac);
+
+    // state of being initialized should live through across session boundaries
+    let result = session.init::<u8>(id, &0xae);
+    assert!(
+        result.is_err(),
+        "initializing more than once should not be allowed even in another session"
+    );
+
+    Ok(())
+}
