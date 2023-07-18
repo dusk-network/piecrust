865a9eedcc29d87974d0c16835be9f8b68d34e9b
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-07-18 19:05:27 +0200

    uplink: add more comprehensive documentation
    We add some crate level docs, ensuring the first page of `docs.rs` is
    not so barebones, and ensure all relevant functions have at least a
    small comment adorning them.
    
    To be more explicit on the feature requirements of each function, we
    also add some compile-time shennanigans for us to detect when `docs.rs`
    is building our crate, to properly display the required features on each
    function and struct.
    
    Resolves: #190
    Resolves: #189
    See-also: #195

--- piecrust-uplink/src/lib.rs
+++ piecrust-uplink/src/lib.rs
@@ -4,13 +4,70 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+//! Piecrust Uplink is the crate for writing WASM smart contracts in Rust
+//! targeting the Piecrust virtual machine.
+//!
+//! A smart contract is a program exposing a collection of functions that
+//! operate on a shared state. These functions can perform a variety of
+//! operations, such as reading or modifying the state, or interacting with the
+//! host.
+//!
+//! # State Model and ABI
+//! Contracts targeting the Piecrust VM represent their state as a [WASM
+//! memory]. The contract is free to represent its data as it sees fit, and may
+//! allocate at will.
+//!
+//! To communicate with the host, both the contract and the host can
+//! emplace data in a special region of this memory called the argument buffer.
+//! The argument buffer is used as both the input from the host and as the
+//! output of the contract. All functions exposed by the contract must follow
+//! the convention:
+//!
+//! ```c
+//! // A function compatible with the piecrust ABI takes in the number of bytes
+//! // written by the host to the argument buffer and returns the number of
+//! // bytes written by the contract.
+//! uint32_t fname(uint32_t arg_len);
+//! ```
+//!
+//! The contract also has some functions available to it, offered by the host
+//! through WASM imports. Examples of these functions include, but are not
+//! limited to:
+//!
+//! - [`call`] to call another contract
+//! - [`emit`] to emit events
+//!
+//! The functions in this crate are wrappers around a particular way of calling
+//! the WASM imports. Take a look at the [externs] for a full view of what is
+//! available.
+//!
+//! # Examples
+//! Some of the contracts used for testing purposes function as good examples.
+//! Take a look at the [contracts/] directory.
+//!
+//! # Features
+//! By default, this crate will include no features and build only the types and
+//! functions available when the ABI is not present. To write a contract one
+//! must use the `abi` feature:
+//!
+//! - `abi` for writing contracts
+//! - `dlmalloc` to using the builtin allocator
+//! - `debug` for writing contracts with debug capabilities such as the
+//!   [`debug!`] macro, and logging panics to stdout
+//!
+//! [WASM memory]: https://wasmbyexample.dev/examples/webassembly-linear-memory/webassembly-linear-memory.rust.en-us.html
+//! [contracts/]: https://github.com/dusk-network/piecrust/tree/main/contracts
+//! [externs]: https://github.com/dusk-network/piecrust/blob/c2dadaa8dec210bdbbc72619a687eb8c6f693877/piecrust-uplink/src/abi/state.rs#L42-L64
+
 #![feature(lang_items)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(feature = "debug", feature(panic_info_message))]
 #![no_std]
 
 extern crate alloc;
 
 #[cfg(feature = "abi")]
+#[cfg_attr(docsrs, doc(cfg(feature = "abi")))]
 mod abi;
 #[cfg(feature = "abi")]
 pub use abi::*;
