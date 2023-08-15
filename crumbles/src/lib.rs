556e35d01e309644c14af6a6b1d5c4f416a15714
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-08-15 21:58:28 +0200

    crumbles: initial commit

---
+++ crumbles/src/lib.rs
@@ -0,0 +1,464 @@
+//! Library for creating and managing 4GiB copy-on-write memory-mapped regions.
+//!
+//! The core functionality is offered by the [`Mmap`] struct, which is a
+//! read-write memory region that keeps track of which pages have been written
+//! to.
+//!
+//! Each `Mmap` is 4GiB in size, and can be backed by physical memory, a set of
+//! files, or a combination of both. Each page is 64KiB in size, meaning that
+//! each mmap contains 65536 pages.
+//!
+//! # Example
+//! ```rust
+//! use crumbles::Mmap;
+//!
+//! let mut mmap = Mmap::new().unwrap();
+//!
+//! // When first created, the mmap is not dirty.
+//! assert_eq!(mmap.dirty_pages().count(), 0);
+//!
+//! mmap[24] = 42;
+//! // After writing a single byte, the page it's on is dirty.
+//! assert_eq!(mmap.dirty_pages().count(), 1);
+//! ```
+//!
+//! # Limitations
+//! This crate currently only builds for 64-bit Unix targets. This is because it
+//! relies on various features of `libc` which are not available in other
+//! targets.
+#![cfg(all(unix, target_pointer_width = "64"))]
+#![deny(missing_docs)]
+#![deny(clippy::pedantic)]
+
+use std::{
+    collections::BTreeSet,
+    fs::File,
+    mem::{self, MaybeUninit},
+    ops::{Deref, DerefMut},
+    os::fd::IntoRawFd,
+    sync::{Once, OnceLock, RwLock},
+    {io, process, ptr, slice},
+};
+
+use libc::{
+    c_int, sigaction, sigemptyset, siginfo_t, sigset_t, ucontext_t,
+    MAP_ANONYMOUS, MAP_FAILED, MAP_FIXED, MAP_PRIVATE, PROT_READ, PROT_WRITE,
+    SA_SIGINFO,
+};
+
+const MEM_SIZE: usize = 0x100_000_000;
+const PAGE_SIZE: usize = 0x10_000;
+
+/// A handle to a 4GiB copy-on-write memory-mapped region that keeps track of
+/// which pages have been written to. Each page is 64KiB in size.
+///
+/// A `Mmap` may be backed by a set of files, physical memory, or a combination
+/// of both. Use [`Mmap::new`] to create a new mmap backed entirely by physical
+/// memory, and [`Mmap::with_files`] to create a new mmap backed by the given
+/// set of files, at the given offsets.
+///
+/// Writes are tracked at the page level. This functions as follows:
+///
+/// - When a region is first mapped, its permissions are set to read-only,
+///   resulting in a segmentation fault when a write is attempted.
+/// - The segmentation fault is then caught using a signal handler, which
+///   proceeds to set the permissions of the page to read-write while also
+///   marking it as dirty.
+///
+/// `Mmap` is [`Sync`] and [`Send`].
+#[derive(Debug)]
+pub struct Mmap(&'static mut MmapInner);
+
+impl Mmap {
+    /// Create a new mmap, backed entirely by physical memory. The memory is
+    /// initialized to all zeros.
+    ///
+    /// # Errors
+    /// If the underlying call to map memory fails, the function will return an
+    /// error.
+    ///
+    /// # Example
+    /// ```rust
+    /// use crumbles::Mmap;
+    ///
+    /// let mmap = Mmap::new().unwrap();
+    /// assert_eq!(mmap[..0x10_000], [0; 0x10_000]);
+    /// ```
+    pub fn new() -> io::Result<Self> {
+        unsafe { Self::with_files(None) }
+    }
+
+    /// Create a new memory, backed by the given files at the given offsets.
+    /// Regions of the memory not covered
+    ///
+    /// # Errors
+    /// If the given files are too large for the 4GiB memory region, or at an
+    /// offset where they wouldn't fit within said region, the function will
+    /// return an error. Also, if any of the files' size or offsets is not a
+    /// multiple of the page size, the function will return an error.
+    ///
+    /// # Safety
+    /// The caller must ensure that the given files are not modified while
+    /// they're mapped. Modifying a file while it's mapped will result in
+    /// *Undefined Behavior* (UB).
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::fs::File;
+    /// use std::io::Read;
+    /// use std::iter;
+    ///
+    /// use crumbles::Mmap;
+    ///
+    /// let mut file = File::open("LICENSE").unwrap();
+    ///
+    /// let mut contents = Vec::new();
+    /// file.read_to_end(&mut contents).unwrap();
+    ///
+    /// let mmap = unsafe { Mmap::with_files(iter::once(Ok((file, 0)))) }.unwrap();
+    /// assert_eq!(mmap[..contents.len()], contents[..]);
+    /// ```
+    pub unsafe fn with_files<I>(files_and_offsets: I) -> io::Result<Self>
+    where
+        I: IntoIterator<Item = io::Result<(File, usize)>>,
+    {
+        let inner = MmapInner::with_files(files_and_offsets)?;
+
+        with_global_map_mut(|global_map| {
+            let inner = Box::leak(Box::new(inner));
+
+            let start_addr = inner.bytes.as_mut_ptr() as usize;
+            let end_addr = start_addr + MEM_SIZE;
+
+            let inner_ptr = inner as *mut _;
+
+            global_map.insert(start_addr..end_addr, inner_ptr as _);
+
+            Ok(Self(inner))
+        })
+    }
+
+    /// Returns an iterator over the dirty pages of the memory, together with
+    /// their offsets.
+    ///
+    /// # Example
+    /// ```rust
+    /// use crumbles::Mmap;
+    ///
+    /// let mut mmap = Mmap::new().unwrap();
+    /// mmap[0x10_000] = 1; // second page
+    ///
+    /// let dirty_pages: Vec<_> = mmap.dirty_pages().collect();
+    ///
+    /// assert_eq!(dirty_pages.len(), 1);
+    /// assert_eq!(dirty_pages[0].1, 0x10_000, "Offset to the first page");
+    /// ```
+    pub fn dirty_pages(&self) -> impl Iterator<Item = (&[u8], usize)> {
+        self.0.dirty_pages.iter().map(move |page_num| {
+            let offset = page_num * PAGE_SIZE;
+            (&self.0.bytes[offset..][..PAGE_SIZE], offset)
+        })
+    }
+}
+
+impl AsRef<[u8]> for Mmap {
+    fn as_ref(&self) -> &[u8] {
+        self.0.bytes
+    }
+}
+
+impl AsMut<[u8]> for Mmap {
+    fn as_mut(&mut self) -> &mut [u8] {
+        self.0.bytes
+    }
+}
+
+impl Deref for Mmap {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        self.0.bytes
+    }
+}
+
+impl DerefMut for Mmap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0.bytes
+    }
+}
+
+// This `Drop` implementation removes the inner memory struct pointer from the
+// global map.
+impl Drop for Mmap {
+    fn drop(&mut self) {
+        with_global_map_mut(|global_map| {
+            unsafe {
+                let inner_ptr = self.0 as *mut MmapInner;
+                let inner = Box::from_raw(inner_ptr);
+
+                let start_addr = inner.bytes.as_mut_ptr() as usize;
+                let end_addr = start_addr + MEM_SIZE;
+
+                global_map.remove(start_addr..end_addr);
+            };
+        });
+    }
+}
+
+type InnerMap = rangemap::RangeMap<usize, usize>;
+
+/// Global memory map. Map from the address range of a mapping to the pointer to
+/// the inner memory struct that contains it.
+static INNER_MAP: OnceLock<RwLock<InnerMap>> = OnceLock::new();
+
+fn with_global_map<T, F>(closure: F) -> T
+where
+    F: FnOnce(&InnerMap) -> T,
+{
+    let global_map = INNER_MAP
+        .get_or_init(|| RwLock::new(InnerMap::new()))
+        .read()
+        .unwrap();
+
+    closure(&global_map)
+}
+
+fn with_global_map_mut<T, F>(closure: F) -> T
+where
+    F: FnOnce(&mut InnerMap) -> T,
+{
+    let mut global_map = INNER_MAP
+        .get_or_init(|| RwLock::new(InnerMap::new()))
+        .write()
+        .unwrap();
+
+    closure(&mut global_map)
+}
+
+/// Contains the actual memory region, together with the set of dirty pages.
+#[derive(Debug)]
+struct MmapInner {
+    bytes: &'static mut [u8],
+    // Set of dirty page numbers.
+    dirty_pages: BTreeSet<usize>,
+}
+
+impl MmapInner {
+    unsafe fn new() -> io::Result<Self> {
+        setup_action();
+
+        let bytes = {
+            let ptr = libc::mmap(
+                ptr::null_mut(),
+                MEM_SIZE,
+                PROT_READ,
+                MAP_PRIVATE | MAP_ANONYMOUS,
+                -1,
+                0,
+            );
+
+            if ptr == MAP_FAILED {
+                return Err(io::Error::last_os_error());
+            }
+
+            slice::from_raw_parts_mut(ptr.cast(), MEM_SIZE)
+        };
+
+        Ok(Self {
+            bytes,
+            dirty_pages: BTreeSet::new(),
+        })
+    }
+
+    pub unsafe fn with_files<I>(files_and_offsets: I) -> io::Result<Self>
+    where
+        I: IntoIterator<Item = io::Result<(File, usize)>>,
+    {
+        let inner = MmapInner::new()?;
+
+        for r in files_and_offsets {
+            let (file, offset) = r?;
+
+            // Since we only build for 64-bit targets, we can safely assume
+            // *neither* truncation *nor* wrapping will happen.
+            #[allow(
+                clippy::cast_possible_truncation,
+                clippy::cast_possible_wrap
+            )]
+            {
+                let file_len = file.metadata()?.len() as usize;
+
+                if offset + file_len > MEM_SIZE {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "File too large for memory region",
+                    ));
+                }
+
+                let ptr = libc::mmap(
+                    inner.bytes.as_mut_ptr().cast(),
+                    file_len,
+                    PROT_READ,
+                    MAP_PRIVATE | MAP_FIXED,
+                    file.into_raw_fd(),
+                    offset as _,
+                );
+
+                if ptr == MAP_FAILED {
+                    return Err(io::Error::last_os_error());
+                }
+            }
+        }
+
+        Ok(inner)
+    }
+
+    unsafe fn set_dirty(&mut self, page_num: usize) -> io::Result<()> {
+        let start_addr = self.bytes.as_ptr() as usize;
+        let dirty_addr = start_addr + page_num * PAGE_SIZE;
+
+        if libc::mprotect(dirty_addr as _, PAGE_SIZE, PROT_READ | PROT_WRITE)
+            != 0
+        {
+            return Err(io::Error::last_os_error());
+        }
+        self.dirty_pages.insert(page_num);
+
+        Ok(())
+    }
+}
+
+impl Drop for MmapInner {
+    fn drop(&mut self) {
+        unsafe {
+            libc::munmap(self.bytes.as_mut_ptr().cast(), MEM_SIZE);
+        }
+    }
+}
+
+static SIGNAL_HANDLER: Once = Once::new();
+
+// Sets up [`segfault_handler`] to handle SIGSEGV, and returns the previous
+// action used to handle it, if any.
+unsafe fn setup_action() -> sigaction {
+    static OLD_ACTION: OnceLock<sigaction> = OnceLock::new();
+
+    SIGNAL_HANDLER.call_once(|| {
+        let mut sa_mask = MaybeUninit::<sigset_t>::uninit();
+        sigemptyset(sa_mask.as_mut_ptr());
+
+        let act = sigaction {
+            sa_sigaction: segfault_handler as _,
+            sa_mask: sa_mask.assume_init(),
+            sa_flags: SA_SIGINFO,
+            #[cfg(target_os = "linux")]
+            sa_restorer: None,
+        };
+        let mut old_act = MaybeUninit::<sigaction>::uninit();
+
+        if libc::sigaction(libc::SIGSEGV, &act, old_act.as_mut_ptr()) != 0 {
+            process::exit(1);
+        }
+
+        // On Apple Silicon for some reason SIGBUS is thrown instead of SIGSEGV.
+        // TODO should investigate properly
+        #[cfg(target_os = "macos")]
+        if libc::sigaction(libc::SIGBUS, &act, old_act.as_mut_ptr()) != 0 {
+            process::exit(2);
+        }
+
+        OLD_ACTION.get_or_init(move || old_act.assume_init());
+    });
+
+    *OLD_ACTION.get().unwrap()
+}
+
+/// Calls the old action that was set to handle `SIGSEGV`
+unsafe fn call_old_action(
+    sig: c_int,
+    info: *mut siginfo_t,
+    ctx: *mut ucontext_t,
+) {
+    let old_act = setup_action();
+
+    // If SA_SIGINFO is set, the old action is a `fn(c_int, *mut siginfo_t, *mut
+    // ucontext_t)`. Otherwise, it's a `fn(c_int)`.
+    if old_act.sa_flags & SA_SIGINFO == 0 {
+        let act: fn(c_int) = mem::transmute(old_act.sa_sigaction);
+        act(sig);
+    } else {
+        let act: fn(c_int, *mut siginfo_t, *mut ucontext_t) =
+            mem::transmute(old_act.sa_sigaction);
+        act(sig, info, ctx);
+    }
+}
+
+unsafe fn segfault_handler(
+    sig: c_int,
+    info: *mut siginfo_t,
+    ctx: *mut ucontext_t,
+) {
+    with_global_map(move |global_map| {
+        let si_addr = (*info).si_addr() as usize;
+
+        if let Some(inner_ptr) = global_map.get(&si_addr) {
+            let inner = &mut *(*inner_ptr as *mut MmapInner);
+
+            let start_addr = inner.bytes.as_mut_ptr() as usize;
+            let page_num = (si_addr - start_addr) / PAGE_SIZE;
+
+            if inner.set_dirty(page_num).is_err() {
+                call_old_action(sig, info, ctx);
+            }
+            return;
+        }
+
+        call_old_action(sig, info, ctx);
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::thread;
+
+    const DIRT: [u8; 2 * PAGE_SIZE] = [42; 2 * PAGE_SIZE];
+    const OFFSET: usize = PAGE_SIZE / 2 + PAGE_SIZE;
+
+    #[test]
+    fn write() {
+        let mut mem =
+            Mmap::new().expect("Instantiating new memory should succeed");
+
+        let slice = &mut mem[OFFSET..][..DIRT.len()];
+        slice.copy_from_slice(&DIRT);
+
+        assert_eq!(slice, DIRT, "Slice should be dirt just written");
+        assert_eq!(mem.dirty_pages().count(), 3);
+    }
+
+    #[test]
+    fn write_multi_thread() {
+        const NUM_THREADS: usize = 8;
+
+        let mut threads = Vec::with_capacity(NUM_THREADS);
+
+        for _ in 0..NUM_THREADS {
+            threads.push(thread::spawn(|| {
+                let mut mem = Mmap::new()
+                    .expect("Instantiating new memory should succeed");
+
+                let slice = &mut mem[OFFSET..][..DIRT.len()];
+                slice.copy_from_slice(&DIRT);
+
+                assert_eq!(slice, DIRT, "Slice should be dirt just written");
+                assert_eq!(mem.dirty_pages().count(), 3);
+            }));
+        }
+
+        threads
+            .drain(..)
+            .for_each(|t| t.join().expect("Thread should exit cleanly"));
+    }
+}
