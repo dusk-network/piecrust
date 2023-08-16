7f1524bb7b98fd601fbaa6fa0c4cb97e80514401
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-08-16 16:20:38 +0200

    crumbles: add license header

--- crumbles/src/lib.rs
+++ crumbles/src/lib.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 //! Library for creating and managing 4GiB copy-on-write memory-mapped regions.
 //!
 //! The core functionality is offered by the [`Mmap`] struct, which is a
