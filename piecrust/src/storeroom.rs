9a44dbc1b05770499d2451099f453fd4c5b1d248
Milosz Muszynski <milosz.muszynski@gmail.com>
2025-01-28 12:40:16 +0100

    piecrust: storeroom wip, specs

--- piecrust/src/storeroom.rs
+++ piecrust/src/storeroom.rs
@@ -15,6 +15,9 @@
 use std::path::{Path, PathBuf};
 use std::{fs, io};
 
+const SHARED_DIR: &str = "SHARED";
+const BLOCKING_FILE_STEM: &str = "BLOCKING";
+
 #[allow(dead_code)]
 pub struct Storeroom {
     main_dir: PathBuf,
@@ -72,24 +75,38 @@ impl Storeroom {
         _contract_id: impl AsRef<str>,
         _version: impl AsRef<str>,
         _postfix: impl AsRef<str>,
-    ) -> io::Result<Vec<u8>> {
-        Ok(vec![])
+    ) -> io::Result<Option<Vec<u8>>> {
+        Ok(Some(vec![]))
     }
 
+    /// If item in version exists and it is not a blocking item it is returned
+    /// If item in version exists and it is blocking item, none is returned
+    /// If item in version does not exist, but in exists in SHARED dir, item from the shared
+    /// dir is returned
     // For memory mapped files we also provide retrieval returning a file path
     pub fn retrieve(
         &self,
         _contract_id: impl AsRef<str>,
         _version: impl AsRef<str>,
         _postfix: impl AsRef<str>,
-    ) -> io::Result<PathBuf> {
-        Ok(PathBuf::new())
+    ) -> io::Result<Option<PathBuf>> {
+        Ok(Some(PathBuf::new()))
     }
 
+    /// Current shared version is the initial state
     pub fn create_version(_version: impl AsRef<str>) -> io::Result<()> {
         Ok(())
     }
 
+    /// When finalizing, items are added or modified in shared version yet:
+    /// for items added to the shared version:
+    ///     for every existing version other than this one, a blocking item is added
+    ///     (blocking item is item that blocks shared item, makes it as if didn't exist)
+    /// for items modified in the shared version:
+    ///     before overwriting the existing item is copied to all existing versions other than this one,
+    ///     but only if it does not exist the corresponding version already
+    /// After the above is done, version can be safely deleted, new versions created in the future
+    /// will use the shared version left by this function as their initial state
     pub fn finalize_version(_version: impl AsRef<str>) -> io::Result<()> {
         Ok(())
     }
