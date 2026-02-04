8183a2cccbff249d5c5902c1f0c2aa839385f69a
Milosz Muszynski <milosz.muszynski@gmail.com>
2026-02-04 18:38:11 +0100

    removed stake from callback

--- piecrust/src/imports.rs
+++ piecrust/src/imports.rs
@@ -320,18 +320,19 @@ pub(crate) fn c(
             &name, &callee_stack_element.contract_id, env.self_contract_id()
         );
         let callback_option = unsafe { &GLOBAL_STATE.get_callback(unique_id) };
-        let should_call_callback = (callee_stack_element.contract_id
+        let should_call_callback = callee_stack_element.contract_id
             == TRANSFER_CONTRACT
             && (name == "deposit"
                 || name == "withdraw"
                 || name == "contract_to_contract"
-                || name == "contract_to_account"))
-            || (callee_stack_element.contract_id == STAKE_CONTRACT
-                && name == "stake_from_contract");
+                || name == "contract_to_account");
+        // || (callee_stack_element.contract_id == STAKE_CONTRACT
+        //     && name == "stake_from_contract");
 
         if callback_option.is_some() && should_call_callback {
             let callback = callback_option.as_ref().unwrap();
             let mut cb = callback.borrow_mut();
+            println!("piecrust imports: calling callback: {}", name);
             let (code, out_v) = cb(
                 env.self_contract_id().to_bytes(),
                 name.to_string(),
