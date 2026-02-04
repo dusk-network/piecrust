83327784f2cac7c155072b16168815cfe83ebedb
moana <moana@dusk.network>
2026-02-04 17:28:15 +0100

    piecrust: Increase gas limit in growth test
    The gc feature adds some overhead for garbage collection infrastructure.
    Double the gas limit from 40M to 80M to accommodate this.

--- piecrust/tests/growth.rs
+++ piecrust/tests/growth.rs
@@ -8,7 +8,7 @@ use piecrust::{ContractData, Error, SessionData, VM, contract_bytecode};
 use piecrust_uplink::ARGBUF_LEN;
 
 const OWNER: [u8; 32] = [0u8; 32];
-const LIMIT: u64 = 40_000_000;
+const LIMIT: u64 = 80_000_000;
 
 #[test]
 fn grow_a_bunch() -> Result<(), Error> {
